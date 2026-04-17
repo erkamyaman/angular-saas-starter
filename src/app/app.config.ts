4be1a06af8a974a78c8475d2e35609b98d742400
Kam <erkamyaman35@gmail.com>
2026-04-17 22:45:18 +0300

    Add PrimeNG and PrimeUix dependencies; configure app for PrimeNG integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,40 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Material from '@primeuix/themes/material';
+import { definePreset } from '@primeuix/themes';
 
 import { routes } from './app.routes';
+
+const AppPreset = definePreset(Material, {
+  semantic: {
+    primary: {
+      50: '{indigo.50}',
+      100: '{indigo.100}',
+      200: '{indigo.200}',
+      300: '{indigo.300}',
+      400: '{indigo.400}',
+      500: '{indigo.500}',
+      600: '{indigo.600}',
+      700: '{indigo.700}',
+      800: '{indigo.800}',
+      900: '{indigo.900}',
+      950: '{indigo.950}'
+    }
+  }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: AppPreset,
+        options: {
+          darkModeSelector: false
+        }
+      }
+    })
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('starter');
+
+  protected onPrimeClick(): void {
+    console.log('PrimeNG button clicked');
+  }
 }
